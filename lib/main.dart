7e0f6bee2d655e81ae5cde49e80d863b0169f14c
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-06-17 22:27:26 +0530

    read data from firebase

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,12 @@ class _MyHomePageState extends State<MyHomePage> {
       "Age" : "20"
     });
     setState(() {
+      database.reference().child("message").once().then((DataSnapshot snapshot)
+      {
+         Map<dynamic,dynamic> data = snapshot.value;
+         print("Values from db: ${snapshot.value}");
+      });
+
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
